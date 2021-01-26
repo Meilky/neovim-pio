f095588b6cede8f56cd8e8090a2fe02cc8ffdf89
meilky <mikaeljean2k3@gmail.com>
2021-01-26 11:47:34 -0500

    create main menu

--- neovim-pio.py
+++ neovim-pio.py
@@ -1,3 +1,23 @@
 import os
+import configparser
 
-print("ok")
+class PioWrapper:
+    def __init__(self,iniPath):
+        self.iniConfig = configparser.ConfigParser()
+        self.iniConfig.read(iniPath)
+
+    def run(self,args:str = ""):
+        os.system("pio run " + args)
+
+class PioMenu:
+    def __init__(self,Wrapper:PioWrapper):
+        self.Wrapper = Wrapper
+
+    def MainMenu(self):
+        print("Your options :")
+        print("")
+
+wrap = PioWrapper("platformio.ini")
+menu = PioMenu(wrap)
+
+menu.MainMenu()
